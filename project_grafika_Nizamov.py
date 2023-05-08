4120eaadaf9f9eb5d2749b39227dc41e89e21cb7
Danis Nizamov <131642172+Dr3nulja@users.noreply.github.com>
2023-05-08 12:33:16 +0300

    upd
    Addded a graphical python code

---
+++ project_grafika_Nizamov.py
@@ -0,0 +1,54 @@
+#02.05.2023
+#graphic design for project
+#Danis Nizamov JPTV22
+
+import tkinter as tk
+window = tk.Tk()
+window.geometry("1280x960")
+window.title("Word maker game")
+
+
+label = tk.Label(text="Word maker game", width=15, height=5, font=("Arial", 17))
+label.pack()
+
+
+label = tk.Label(text="_"*1000)
+label.pack()
+
+
+label = tk.Label(text="Letters: ", font=("Arial", 15))
+label.pack(anchor="w")
+
+
+label = tk.Label(text="_"*1000)
+label.pack()
+
+entry = tk.Entry(width=17, font=("Arial", 24))
+entry.pack(padx=6, pady=6)
+
+def myClick():
+    entry = tk.Entry(width=17, font=("Arial", 24))
+    entry.pack(padx=6, pady=6)
+    button = tk.Button(text="Next", command=myClick, font=("Arial", 12))
+    button.pack(pady=10)
+
+
+button = tk.Button(text="Next", command=myClick, font=("Arial", 12))
+button.pack(pady=10)
+
+
+button = tk.Button(text="Save", font=("Arial", 15))
+button.place(x="25", y="15")
+button.pack(anchor="se")
+
+
+def myQuit():
+    window.destroy()
+    window.quit()
+
+
+button = tk.Button(text="Quit", font=("Arial", 15), command=myQuit)
+button.place(x="25", y="35")
+button.pack(anchor="se")
+
+input()
